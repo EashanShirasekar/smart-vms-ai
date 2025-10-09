65f8f1888f06558079b455c199c25eb6c4399d9e
Pranav777-del <pranav.m3k@gmail.com>
2025-10-09 11:02:57 +0530

    Model Changes and more Images Added

--- ai_service/register_face.py
+++ ai_service/register_face.py
@@ -10,7 +10,7 @@ if len(sys.argv) < 4:
 name, emp_id, img_path = sys.argv[1], sys.argv[2], sys.argv[3]
 
 # Build embedding using DeepFace
-embedding_obj = DeepFace.represent(img_path=img_path, model_name="Facenet")
+embedding_obj = DeepFace.represent(img_path=img_path, model_name="ArcFace")
 embedding = np.array(embedding_obj[0]["embedding"])
 
 save_face(name, emp_id, embedding)
